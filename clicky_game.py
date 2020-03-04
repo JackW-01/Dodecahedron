9143b4f9b2d964c252970d7c69fd43d255614a5f
JackW-01 <60620735+JackW-01@users.noreply.github.com>
2020-03-03 20:58:00 -0500

    Update clicky_game.py
    Added missing
    Optimized code a little bit

--- clicky_game.py
+++ clicky_game.py
@@ -1,5 +1,4 @@
 from random import randint
-from random import seed
 import math
 import arcade
 
@@ -7,17 +6,18 @@ WIDTH = 800
 HEIGHT = 600
 
 window = arcade.Window(WIDTH, HEIGHT, "Bootleg Aimbooster")
-arcade.set_background_color(arcade.color.WHITE)
+arcade.set_background_color(arcade.color.LIGHT_SLATE_GRAY)
 
 # Initialize your variables here
-cx = WIDTH // 2 + 30
-cy = HEIGHT // 2 + 30
+cx = randint(15, WIDTH-15)
+cy = randint(15, HEIGHT-30)
 cr = 7
-cx_ = WIDTH // 2 - 30
-cy_ = HEIGHT // 2 - 30
+cx_ = randint(15, WIDTH-15)
+cy_ = randint(15, HEIGHT-30)
 cr_ = 7
 score = 0
-textScore = f"Score: {score}"
+misses = 10
+textScore: str
 instructions = "Click the Circles! (Speed = more points!)"
 
 
@@ -32,26 +32,29 @@ def game_loop():
     global cr_
     global score
     global textScore
+    global misses
     # update your variables here.
-    textScore = f"Score: {score}"
-    cr += 0.065
+    textScore = f"Score: {score}        Misses Left: {misses}"
+    if cr >= 25:
+        misses -= 1
+        cr = 7
+        cx = randint(15, WIDTH-15)
+        cy = randint(15, HEIGHT-30)
+    elif cr_ >= 25:
+        misses -= 1
+        cr_ = 7
+        cx_ = randint(15, WIDTH-15)
+        cy_ = randint(15, HEIGHT-30)
     cr_ += 0.065
-    if cr >= 25 or cr_ >= 25:
-        score -= 4
-        if cr >= 25:
-            cr = 7
-            cx = randint(15, WIDTH-15)
-            cy = randint(15, HEIGHT-30)
-        elif cr_ >= 25:
-            cr_ = 7
-            cx_ = randint(15, WIDTH-15)
-            cy_ = randint(15, HEIGHT-30)
+    cr += 0.065
+    if misses < 0:
+        exit()
     # Draw things here.
     arcade.start_render()
     arcade.draw_circle_filled(cx, cy, cr, arcade.color.ORANGE_PEEL)
     arcade.draw_circle_filled(cx_, cy_, cr_, arcade.color.RED_ORANGE)
-    arcade.draw_text(textScore, 50, HEIGHT-20, arcade.color.BLACK, 14)
-    arcade.draw_text(instructions, 500, HEIGHT-20, arcade.color.BLACK, 12)
+    arcade.draw_text(textScore, 10, HEIGHT-20, arcade.color.BLACK, 14)
+    arcade.draw_text(instructions, 510, HEIGHT-20, arcade.color.BLACK, 12)
 
 
 @window.event
@@ -64,12 +67,13 @@ def on_mouse_press(mox, moy, button, modifiers):
     global cy_
     global cr_
     global score
-    sideA = max(cx - mox, mox - cx)
-    sideB = max(cy - moy, moy - cy)
-    distance = math.sqrt(sideA * sideA + sideB * sideB)
-    sideA = max(cx_ - mox, mox - cx_)
-    sideB = max(cy_ - moy, moy - cy_)
-    distance_ = math.sqrt(sideA * sideA + sideB * sideB)
+    global misses
+    sideA = abs(cx - mox)
+    sideB = abs(cy - moy)
+    distance = math.sqrt(sideA ** 2 + sideB ** 2)
+    sideA = abs(cx_ - mox)
+    sideB = abs(cy_ - moy)
+    distance_ = math.sqrt(sideA ** 2 + sideB ** 2)
     # Update variables
     if distance <= cr:
         if cr <= 14:
@@ -88,7 +92,7 @@ def on_mouse_press(mox, moy, button, modifiers):
         cx_ = randint(15, WIDTH-15)
         cy_ = randint(15, HEIGHT-30)
     else:
-        score -= 2
+        misses -= 1
 
 
 arcade.run()
